f827f7f7750bb5a9e8153ab0b866af08f2c6590e
Eric Rouse <eric.rouse@gmail.com>
2022-03-05 10:36:37 -0600

    Decode example

--- main.go
+++ main.go
@@ -13,10 +13,14 @@ type person struct {
 func main() {
 	http.HandleFunc("/encode", foo)
 	http.HandleFunc("/decode", bar)
-	http.ListenAndServe(":8080", nil)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
+func foo(w http.ResponseWriter, _ *http.Request) {
 	p1 := person{
 		First: "Jenny",
 	}
@@ -27,6 +31,13 @@ func foo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func bar(w http.ResponseWriter, r *http.Request) {
+func bar(_ http.ResponseWriter, r *http.Request) {
+	var p1 person
 
+	err := json.NewDecoder(r.Body).Decode(&p1)
+	if err != nil {
+		log.Println("Decoded bad data", err)
+	}
+
+	log.Println("Person", p1)
 }
