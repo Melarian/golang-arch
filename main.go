51ef766347436c6d8f60fdd1f842f4c4cd2eca84
Eric Rouse <eric.rouse@gmail.com>
2022-03-06 09:26:29 -0600

    HMAC functions

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"fmt"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+var key []byte
+
 func main() {
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
+
 	pass := "123456789"
 
 	hashedPass, err := hashPassword(pass)
@@ -26,7 +34,7 @@ func main() {
 func hashPassword(password string) ([]byte, error) {
 	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, fmt.Errorf("Error while generating bcrypt hash from password: %w", err)
+		return nil, fmt.Errorf("error while generating bcrypt hash from password: %w", err)
 	}
 
 	return bs, nil
@@ -35,8 +43,31 @@ func hashPassword(password string) ([]byte, error) {
 func comparePassword(password string, hashedPass []byte) error {
 	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
 	if err != nil {
-		return fmt.Errorf("Invalid password: %w", err)
+		return fmt.Errorf("invalid password: %w", err)
 	}
 
 	return nil
+}
+
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(sha512.New, key)
+
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("error in signMessage while hashing message: %w", err)
+	}
+
+	sig := h.Sum(nil)
+
+	return sig, nil
+}
+
+func checkSig(msg, sig []byte) (bool, error) {
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("error in checkSig while getting signture of message: %w", err)
+	}
+
+	same := hmac.Equal(newSig, sig)
+	return same, nil
 }
